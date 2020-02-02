a9eb59194e321018642b88a6431684c6041d1e7e
namjose <namjackson123@gmail.com>
2020-02-03 09:48:07 +1100

    Update download and upload functionality

---
+++ src/components/custom-button/DownloadButton.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Typography from '@material-ui/core/Typography'
+import { Grid, TextField, Button } from '@material-ui/core'
+import { CSVLink, CSVDownload } from 'react-csv'
+
+const defaultHeaders = [
+  { label: 'First Name', key: 'firstname' },
+  { label: 'Last Name', key: 'lastname' },
+  { label: 'Email', key: 'email' }
+]
+
+const defaultData = [
+  { firstname: 'Ahmed', lastname: 'Tomi', email: 'ah@smthing.co.com' },
+  { firstname: 'Raed', lastname: 'Labes', email: 'rl@smthing.co.com' },
+  { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' }
+]
+
+export default function DownloadButton({
+  headers = defaultHeaders,
+  data = defaultData
+}) {
+  const csvLink = React.useRef()
+
+  const handleDowload = () => {
+    csvLink.current.link.click()
+  }
+
+  return (
+    <>
+      <Button variant="contained" color="primary" onClick={handleDowload}>
+        Download Sample Vector
+      </Button>
+      <CSVLink
+        data={data}
+        headers={headers}
+        filename="sample-vector.csv"
+        target="_blank"
+        ref={csvLink}
+        hidden
+      />
+    </>
+  )
+}
--- src/components/tabs/UploadPartialView.js
+++ src/components/tabs/UploadPartialView.js
@@ -1,14 +1,85 @@
 import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography'
 import { Grid, TextField, Button } from '@material-ui/core'
+import { CSVLink, CSVDownload } from 'react-csv'
+import ReactFileReader from 'react-file-reader'
+import Papa from 'papaparse'
+
+const headers = [
+  { label: 'First Name', key: 'firstname' },
+  { label: 'Last Name', key: 'lastname' },
+  { label: 'Email', key: 'email' }
+]
+
+const data = [
+  { firstname: 'Ahmed', lastname: 'Tomi', email: 'ah@smthing.co.com' },
+  { firstname: 'Raed', lastname: 'Labes', email: 'rl@smthing.co.com' },
+  { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' }
+]
 
 export const UploadPartialView = ({ classes }) => {
-  const [upload, setUpload] = useState('bank_partial_view.csv')
+  const [upload, setUpload] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [fileUpload, setFileUpload] = useState({
+    fileName: '',
+    totalRow: 0,
+    totalColumns: 0
+  })
+
+  const csvLink = React.useRef()
+
+  const handleDowload = () => {
+    csvLink.current.link.click()
+  }
+
+  const handleFiles = files => {
+    setIsLoading(true)
+
+    const csvFile = files[0]
+    console.log(csvFile)
+
+    Papa.parse(csvFile, {
+      complete: result => {
+        var data = result.data
+        const {
+          meta: { fields },
+          meta
+        } = result
+        console.log({ data, meta })
+        setIsLoading(false)
+        setFileUpload({
+          fileName: csvFile.name,
+          totalColumns: fields.length,
+          totalRow: data.length
+        })
+      },
+      header: true
+    })
+
+    let formData = new FormData()
+    formData.append('csvFile', csvFile)
+    formData.append('name', csvFile.name)
+
+    // post to upload file
+  }
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} container direction="row" justify="space-between">
         <Typography variant="h5">Partial View</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" color="primary" onClick={handleDowload}>
+          Download Sample Vector
+        </Button>
+        <CSVLink
+          data={data}
+          headers={headers}
+          filename="sample-vector.csv"
+          target="_blank"
+          ref={csvLink}
+          hidden
+        />
       </Grid>
       <Grid item xs={12} lg={10}>
         <TextField
@@ -20,14 +91,7 @@ export const UploadPartialView = ({ classes }) => {
         />
       </Grid>
       <Grid item xs={12} lg={2}>
-        <input
-          accept="image/*"
-          className={classes.input}
-          id="contained-button-file"
-          multiple
-          type="file"
-        />
-        <label htmlFor="contained-button-file">
+        <ReactFileReader handleFiles={handleFiles} fileTypes={'.csv'}>
           <Button
             variant="contained"
             color="primary"
@@ -36,16 +100,23 @@ export const UploadPartialView = ({ classes }) => {
           >
             Upload Partial View
           </Button>
-        </label>
+        </ReactFileReader>
       </Grid>
-      {upload && (
+      {isLoading && (
+        <Grid item xs={12}>
+          <Typography>Loading...</Typography>
+        </Grid>
+      )}
+      {fileUpload.fileName && (
         <Grid item xs={12}>
           <Typography variant="h6">Brief Description</Typography>
-          <Typography variant="p">Data Name: bank_partial_view</Typography>
+          <Typography variant="p">Data Name: {fileUpload.fileName}</Typography>
           <br />
-          <Typography variant="p">Total Rows: 1.000.000</Typography>
+          <Typography variant="p">Total Rows: {fileUpload.totalRow}</Typography>
           <br />
-          <Typography variant="p">Total Columns: 10</Typography>
+          <Typography variant="p">
+            Total Columns: {fileUpload.totalColumns}
+          </Typography>
         </Grid>
       )}
     </Grid>
